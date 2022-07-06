7d2a4f5f6b63115f3b0d6acda8b3f2e06cccc887
aLHJ123 <100500957+alhj123@users.noreply.github.com>
2022-07-06 17:37:26 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        s.nextLine();
+    }
+}
